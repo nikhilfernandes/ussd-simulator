ac83b2cd0f793dc5d6573c0149f7afb242dfb283
Nikhil <fernandes.nikhil@gmail.com>
2015-06-28 21:26:17 +0530

    flow for multiple account balance

--- balance_inquiry.py
+++ balance_inquiry.py
@@ -15,13 +15,30 @@ class BalanceInquiry:
 
     def step_three(self):
         if (self.balance_for == "1"):
-            self.account_number = input("Please choose an account: \n 1. 00031xxxxx1234 \n 2. 00090xxxxx5678\n")
-            if (self.account_number == "1"):
-                print("Your account balance is: \n Rs.2000.00")
-                print("\n")
-                bank_flow = BankFlow()
-                bank_flow.menu()
+            self.single_account_balance()
+        elif (self.balance_for == "2"):
+            self.show_account_balance("Your account balance for 00031xxxxx1234 is: \n Rs.2000.00\n Your account balance for 00090xxxxx5678 is: \n Rs.1000.00")
+        else:
+            self.show_account_balance("Wrong Selection")
 
+
+                
+
+    def show_account_balance(self, message):
+        print(message)
+        print("\n")
+        bank_flow = BankFlow()
+        bank_flow.menu()
+
+
+    def single_account_balance(self):
+        self.account_number = input("Please choose an account: \n 1. 00031xxxxx1234 \n 2. 00090xxxxx5678\n")            
+        if (self.account_number == "1"):
+            self.show_account_balance("Your account balance is: \n Rs.2000.00")               
+        elif (self.account_number == "2"):
+            self.show_account_balance("Your account balance is: \n Rs.1000.00")                
+        else:
+            self.show_account_balance("Wrong Selection")                                
 
 
 
